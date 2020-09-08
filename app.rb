a940dced9e309fb5dbbcbef32e8fa38542956d63
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-09-08 04:03:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,15 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :new 
+    end
+    
+    post '/pirates' do
+      
+      erb :show 
+    end
+      
 
   end
 end
