702ce9fea49781a2ee775e07b34c34d7feea46f9
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-09-08 05:35:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require './environment'
 
-module FormsLab
   class App < Sinatra::Base
 
+    # code other routes/actions here
     get '/' do
       erb :root
     end
@@ -12,13 +12,11 @@ module FormsLab
     end
 
     post '/pirates' do
-      #raise params.inspect
       @pirate = Pirate.new(params[:pirate])
 
       params[:pirate][:ships].each do |details|
         Ship.new(details)
       end
-
       @ships = Ship.all
 
       erb :'pirates/show'
